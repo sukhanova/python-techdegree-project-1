d8e717eee15c1e8d2514f0c9e30ecfc57b1a7b76
Vira Sukhanova <vira@Viras-MacBook-Pro.local>
2021-03-10 16:36:15 -0600

    modified comment

--- guessing_game.py
+++ guessing_game.py
@@ -3,10 +3,6 @@ Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
 
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
 """
 
 import random
